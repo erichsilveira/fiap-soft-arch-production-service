2b6e86aaa1bb05cf0fda41b7a4c3cf3175f281f2
Guilherme Graf <guilherme.graf@posherapp.com>
2024-06-15 22:23:50 -0300

    fix(owasp_vulnerabilities)

--- src/main/java/com/fiap/production/interfaces/dto/AdditionalItemCreationRequest.java
+++ src/main/java/com/fiap/production/interfaces/dto/AdditionalItemCreationRequest.java
@@ -2,11 +2,13 @@ package com.fiap.production.interfaces.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
-public record AdditionalItemCreationRequest(@NotBlank String name, String description,
-                                            @NotBlank String productCategoryId,
+public record AdditionalItemCreationRequest(@NotBlank @Size(min = 1, max = 100) String name,
+                                            @Size(min = 1, max = 1000) String description,
+                                            @NotBlank @Size(min = 1, max = 100) String productCategoryId,
                                             @NotNull BigDecimal price) {
 
 }
